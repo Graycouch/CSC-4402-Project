79b4dd5ead9b6d570124bd78a52c28e416d252f8
Abdel Rahman Mansour <65471490+Graycouch@users.noreply.github.com>
2023-03-15 16:13:01 -0500

    Added get by ID endpoints

--- node-rest-api/src/Routes/candidate.js
+++ node-rest-api/src/Routes/candidate.js
@@ -13,6 +13,22 @@ router.get("/", async (req, res) => {
     })
 });
 
+router.get("/get/:id", async (req, res) => {
+    if (!req.params.id) {
+        res.status(400).send("You need to provide an ID for the candidate you want to get!");
+    } else {
+        let query = `SELECT * FROM candidate WHERE ID = ${req.params.id}`;
+
+        database.query(query, function (error, data) {
+            if (error) {
+                res.status(500).send("An error occurred executing this query!");
+            } else {
+                res.status(200).json(data);
+            }
+        })
+    }
+});
+
 router.delete("/delete/:id", async (req, res) => {
     if (!req.params.id) {
         res.status(400).send("You need to provide an ID for the candidate you want to delete!");
