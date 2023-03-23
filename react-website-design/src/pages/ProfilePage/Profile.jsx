27ea373e9706190b9e3af38ac0bf7367bf480c53
Abdel Rahman Mansour <abdelrahmanmansour@outlook.com>
2023-03-23 16:08:07 -0500

    Fixed up sign up page and fixed an issue in the server that was causing strings not work

---
+++ react-website-design/src/pages/ProfilePage/Profile.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Voters extends Component {
+    render() {
+        return (
+            <div>
+                <h1 style={{ textAlign: 'center' }}>
+                    Profile
+                </h1>
+            </div>
+        )
+    }
+}
--- react-website-design/src/pages/RegisterPage/Register.jsx
+++ react-website-design/src/pages/RegisterPage/Register.jsx
@@ -5,12 +5,15 @@ import "./Register.css"
 function Register() {
     const handleSignUpClick = (e) => {
         e.preventDefault();
-        const ID = document.getElementById("voterID").value;
-        const party_ID = document.getElementById("partyID").value;
 
         axios.post(`http://localhost:8080/voter/create`, {
-            ID: ID,
-            party_ID: party_ID
+            ID: document.getElementById("ID").value,
+            first_name: document.getElementById("first_name").value,
+            last_name: document.getElementById("last_name").value,
+            DOB: document.getElementById("DOB").value,
+            SSN: document.getElementById("SSN").value,
+            state: document.getElementById("state").value,
+            district_number: document.getElementById("district_number").value
         }).then((response) => {
             console.log(response);
             setGlobalState("isRegistering", false);
@@ -36,8 +39,13 @@ function Register() {
 
                 <div className="LoginRight">
                     <form className="RegisterBox" onSubmit={handleSignUpClick}>
-                        <input placeholder="Voter ID" required className="LoginInput" id="voterID" />
-                        <input placeholder="Party ID" required className="LoginInput" id="partyID" />
+                        <input placeholder="Voter ID" required className="LoginInput" id="ID" />
+                        <input placeholder="First Name" required className="LoginInput" id="first_name" />
+                        <input placeholder="Last Name" required className="LoginInput" id="last_name" />
+                        <input placeholder="Date of Birth: MM/DD/YYYY" required className="LoginInput" id="DOB" />
+                        <input placeholder="Social Security Number: XXX-XX-XXXX" required className="LoginInput" id="SSN" />
+                        <input placeholder="State (Example: Louisiana)" required className="LoginInput" id="state" />
+                        <input placeholder="District Number: XXX" required className="LoginInput" id="district_number" />
 
                         <button className="LoginButton">Sign Up</button>
 
