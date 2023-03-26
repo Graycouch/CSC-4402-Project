288a95d3e5520add7c9095060f0646e97992a18f
Abdel Rahman Mansour <65471490+Graycouch@users.noreply.github.com>
2023-03-26 17:00:10 -0500

    Added a bunch of new endpoints so that the frontend can communicate with the server effectively

--- node-rest-api/src/Routes/election.js
+++ node-rest-api/src/Routes/election.js
@@ -33,6 +33,42 @@ router.get("/get/:id", async (req, res) => {
     }
 });
 
+router.get("/get-candidates/:id", async (req, res) => {
+    if (!req.params.id) {
+        res.status(400).send("You need to provide an ID for the election you want to get!");
+    } else {
+        let query = `SELECT * FROM candidate INNER JOIN running_for ON candidate.ID = running_for.candidate_ID where running_for.election_ID = ${req.params.id}`;
+
+        database.query(query, function (error, data) {
+            res.header("Access-Control-Allow-Origin", "*");
+
+            if (error) {
+                res.status(500).send("An error occurred executing this query!");
+            } else {
+                res.status(200).json(data);
+            }
+        })
+    }
+});
+
+router.get("/get-votes/:id", async (req, res) => {
+    if (!req.params.id) {
+        res.status(400).send("You need to provide an ID for the election you want to get!");
+    } else {
+        let query = `SELECT candidate.ID, candidate.first_name, candidate.last_name, count(ID) AS '# of Votes' FROM candidate INNER JOIN votes_in ON candidate.ID = votes_in.candidate_ID where votes_in.election_ID = ${req.params.id} GROUP BY ID`;
+
+        database.query(query, function (error, data) {
+            res.header("Access-Control-Allow-Origin", "*");
+
+            if (error) {
+                res.status(500).send("An error occurred executing this query!");
+            } else {
+                res.status(200).json(data);
+            }
+        })
+    }
+});
+
 router.delete("/delete/:id", async (req, res) => {
     if (!req.params.id) {
         res.status(400).send("You need to provide an ID for the election you want to delete!");
--- sql-relational-database/voter_registration.sql
+++ sql-relational-database/voter_registration.sql
@@ -16,8 +16,6 @@ INSERT INTO party VALUES (5, 'Forward');
 INSERT INTO party VALUES (9, 'Independent');
 INSERT INTO party VALUES (99, 'Illuminati');
 
--- SELECT * FROM party;
-
 CREATE TABLE voter(
 ID VARCHAR(20) NOT NULL,
 first_name VARCHAR(20),
@@ -37,8 +35,6 @@ INSERT INTO voter VALUES ('891977906', 'Abdel Rahman', 'Mansour', '123-45-6789',
 INSERT INTO voter VALUES ('12345', 'George', 'Buras', '987-65-4321', '03/13/2000', 'gburas4@lsu.edu', '111-222-3333', 9, 14, 'Louisiana');
 INSERT INTO voter VALUES ('087687', 'Peter', 'Parker', '624-65-5342', '06/27/1984', 'pete@gmail.edu', '274-234-5134', 5, 13, 'New York');
 INSERT INTO voter VALUES ('2642113', 'Peter', 'Pan', '134-34-6433', '05/14/1984', 'pan@gmail.edu', '34t-24-6344', 3, 5, 'Louisiana');
-
--- SELECT * FROM voter;
 
 CREATE TABLE candidate(
 ID VARCHAR(20) NOT NULL,
@@ -70,9 +66,6 @@ INSERT INTO candidate VALUES ('723', 'Ben', 'Jake', '244-16-7342', '09/22/1989',
 INSERT INTO candidate VALUES ('245', 'Ashley', 'Jakobs', '272-25-4854', '12/21/1983', 'Ash@gmail.com', '346-243-2451', 4, 2, 'Louisiana', 'Secretary', null, null, null, null, 'Visit my website for my 100 step plan', 'If the world dies, we die');
 INSERT INTO candidate VALUES ('835', 'Brice', 'Samuel', '724-25-2346', '08/30/1983', 'brice@gmail.com', '745-542-7245', 5, 16, 'Louisiana', 'Thinker', 'We should fix the system', 'I have a plan', 'You can count on me', 'Do not worry', 'Let us figure it out together', 'Let us solve the issues');
 
--- SELECT * FROM candidate;
-
-
 CREATE TABLE election(
 ID VARCHAR(20) NOT NULL,
 office VARCHAR(30), -- Office to be won in the election (e.g. President)
@@ -84,8 +77,6 @@ PRIMARY KEY (ID)
 INSERT INTO election VALUES ('1', 'President', '05/19/2023', null);
 INSERT INTO election VALUES ('2', 'Secretary', '05/19/2023', null);
 INSERT INTO election VALUES ('3', 'Puppet Master', '05/19/1980', '947');
-
--- SELECT * FROM election;
 
 CREATE TABLE running_for(
 candidate_ID VARCHAR(20),
@@ -103,12 +94,10 @@ INSERT INTO running_for VALUES ('723', '1');
 INSERT INTO running_for VALUES ('999', '2');
 INSERT INTO running_for VALUES ('835', '2');
 
--- SELECT * from running_for;
-
 CREATE TABLE votes_in(
 voter_ID VARCHAR(20) NOT NULL,
 election_ID VARCHAR(20) NOT NULL,
-vote_ID VARCHAR(20), -- ID of the candidate who the voter wants to win the election
+candidate_ID VARCHAR(20), -- ID of the candidate who the voter wants to win the election
 PRIMARY KEY (voter_ID, election_ID),
 FOREIGN KEY (voter_ID) REFERENCES voter(ID),
 FOREIGN KEY (election_ID) REFERENCES election(ID)
@@ -119,8 +108,6 @@ INSERT INTO votes_in VALUES ('12345', '1', '723');
 INSERT INTO votes_in VALUES ('12345', '2', '835');
 INSERT INTO votes_in VALUES ('087687', '2', '835');
 INSERT INTO votes_in VALUES ('087687', '1', '999');
-
--- SELECT * FROM votes_in;
 
 CREATE TABLE favorites(
 voter_ID VARCHAR(20) NOT NULL,
@@ -136,16 +123,14 @@ INSERT INTO favorites VALUES ('12345', '245');
 INSERT INTO favorites VALUES ('087687', '999');
 INSERT INTO favorites VALUES ('087687', '835');
 
--- SELECT * FROM favorites;
-
--- The list of candidates who are running for a particular election
--- SELECT first_name, last_name FROM candidate INNER JOIN running_for ON candidate.ID = running_for.candidate_ID where running_for.election_ID = 1;
-
--- How many votes each candidate got in an election
--- SELECT candidate.ID, candidate.first_name, candidate.last_name, count(ID) AS '# of Votes'  FROM candidate INNER JOIN votes_in ON candidate.ID = votes_in.vote_ID where votes_in.election_ID = 1 GROUP BY ID;
-
 -- Voter party membership list
 -- SELECT voter.ID, first_name, last_name, party_name FROM voter LEFT OUTER JOIN party ON voter.party_ID = party.ID;
 
 -- A voter's favorite candidates
 -- SELECT candidate.ID, first_name, last_name FROM candidate INNER JOIN favorites ON candidate.ID = favorites.candidate_ID WHERE voter_ID = '12345';
+
+-- The list of candidates who are running for a particular election
+-- SELECT first_name, last_name FROM candidate INNER JOIN running_for ON candidate.ID = running_for.candidate_ID where running_for.election_ID = 1;
+
+-- How many votes each candidate got in an election
+-- SELECT candidate.ID, candidate.first_name, candidate.last_name, count(ID) AS '# of Votes' FROM candidate INNER JOIN votes_in ON candidate.ID = votes_in.candidate_ID where votes_in.election_ID = 1 GROUP BY ID;
