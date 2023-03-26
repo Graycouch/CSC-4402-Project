43751a26e804dc6b21ef7dc756bdaae26c786370
Moroccxn <aelkha5@lsu.edu>
2023-03-26 15:25:05 -0500

    Fixed Candidate Data not appearing with Abdel

--- react-website-design/src/pages/CandidatesPage/Candidates.jsx
+++ react-website-design/src/pages/CandidatesPage/Candidates.jsx
@@ -25,7 +25,7 @@ function Candidates() {
     async function getCandidates() {
         try {
             const res = await axios.get('http://localhost:8080/candidate');
-            // console.log(res.data);
+            console.log(res.data);
             setCandidates(res.data);
 
         }
@@ -37,18 +37,7 @@ function Candidates() {
     useEffect(() => {
         getCandidates();
     }, []);
-    /* Hard-coded candidate data */
-    // const candidates = [
-    //     {first_name: "Ashley", last_name: "Jakobs", party_ID: "Green", image: './Images/Ashley-Jakobs.png', state: 'NY', DOB: '44', current_job: 'Lawyer'},
-    //     {first_name: "Ben", last_name: "Jake", party_ID: "Dem.", image: './Images/Ben-Jake.png', state: 'LA', DOB: '57', current_job: 'Doctor'},
-    //     {first_name: "Billy", last_name: "Bob", party_ID: "Ind.", image: './Images/Billy-Bob.png', state: 'KS', DOB: '49', current_job: 'Farmer'},
-    //     {first_name: "Brice", last_name: "Samuel", party_ID: "Lib.", image: './Images/Brice-Samuel.png', state: 'TX', DOB: '60', current_job: 'Governor'},
-    //     {first_name: "Dan", last_name: "Jackson", party_ID: "Forw.", image: './Images/Dan-Jackson.png', state: 'FL', DOB: '63', current_job: 'Senator'},
-    //     {first_name: "Joe", last_name: "Mama", party_ID: "Rep.", image: './Images/Joe-Mama.png', state: 'TX', DOB: '68', current_job: 'Lobbyist'},
-    //     {first_name: "Specimen", last_name: "#0911", party_ID: "Illuminati", image: './Images/Specimen.png', state: 'Mars', DOB: '3225', current_job: 'Mind Control'},
-    //     {first_name: "Default", last_name: "Candidate", party_ID: "N/A", image: './Images/default.png', state: '*', DOB: '0', current_job: '-----'}
-    // ];
-
+   
     // Function to render cards
     function showCandidateCards() {
         return (
