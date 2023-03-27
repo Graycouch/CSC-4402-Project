d755815a565c78dc65bc1fbe0d59ca9799845b6d
John Hudnall <114320176+john-hudnall@users.noreply.github.com>
2023-03-26 21:45:11 -0500

    Minor Changes to Profile

--- react-website-design/src/pages/ProfilePage/Profile.jsx
+++ react-website-design/src/pages/ProfilePage/Profile.jsx
@@ -11,30 +11,36 @@ export default class Voters extends Component {
       render() {
         return (
             <div>
-        <h1 style={{ textAlign: 'center' }}>
-          Candidate Profile
-        </h1>
-        <div className="profile-container">
-        <img className="profile-picture" src="goat.png" alt="Profile" />
-        </div>
-        <div className="profile-container">
-          <div className="profile-info">
-            <p><strong>Voter ID: 123456</strong></p>
-            <p><strong>Name: </strong>John Doe</p>
-            <p><strong>Date of Birth: </strong>01/01/1990</p>
-            <p><strong>Political Party: </strong>Democrat</p>
-            <p><strong>Current Job: </strong>State Senator</p>
-            <p><strong>District Number: </strong>1</p>
-            <p><strong>State: </strong>California</p>
-            <select value={this.state.selectedOpinion} onChange={(e) => this.setState({ selectedOpinion: e.target.value })}>
-              <option value="">Political Opinions</option>
-              {this.state.opinions.map((opinion, index) => (
-                <option key={index} value={opinion}>{opinion}</option>
-              ))}
-            </select>
-          </div>
-        </div>
-      </div> 
+                <div className="profile-container">
+                    <h1>
+                       My Profile
+                    </h1>
+                </div>
+
+            <div>
+                <p>
+                <img className="profile-picture" src="goat.png" alt="Profile"/>
+                </p>
+            </div>
+
+            <div className="profile-container">
+                <div className="profile-info">
+                    <p><strong>Voter ID: 123456</strong></p>
+                    <p><strong>Name: </strong>John Doe</p>
+                    <p><strong>Date of Birth: </strong>01/01/1990</p>
+                    <p><strong>Political Party: </strong>Democrat</p>
+                    <p><strong>Current Job: </strong>State Senator</p>
+                    <p><strong>District Number: </strong>1</p>
+                    <p><strong>State: </strong>California</p>
+                    <select value={this.state.selectedOpinion} onChange={(e) => this.setState({ selectedOpinion: e.target.value })}>
+                        <option value="">Political Opinions</option>
+                        {this.state.opinions.map((opinion, index) => (
+                            <option key={index} value={opinion}>{opinion}</option>
+                        ))}
+                    </select>
+                </div>
+            </div>
+            </div> 
         );
       }
 }
