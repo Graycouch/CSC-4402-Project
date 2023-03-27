dab1779c4e8c158160416b0bca80f187c840549f
John Hudnall <114320176+john-hudnall@users.noreply.github.com>
2023-03-26 20:25:21 -0500

    Rough Outline for Profile Page

--- react-website-design/src/pages/ProfilePage/Profile.jsx
+++ react-website-design/src/pages/ProfilePage/Profile.jsx
@@ -1,13 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, Picker } from 'react';
+import "./Profile.css";
 
 export default class Voters extends Component {
-    render() {
+    state = {
+        opinions: ['Opinion 1', 'Opinion 2', 'Opinion 3'],
+        selectedOpinion: ''
+      }
+    
+      render() {
         return (
             <div>
-                <h1 style={{ textAlign: 'center' }}>
-                    Profile
-                </h1>
-            </div>
-        )
-    }
+        <h1 style={{ textAlign: 'center' }}>
+          Candidate Profile
+        </h1>
+        <div className="profile-container">
+        <img className="profile-picture" src="goat.png" alt="Profile" />
+        </div>
+        <div className="profile-container">
+          <div className="profile-info">
+            <p><strong>Voter ID: 123456</strong></p>
+            <p><strong>Name: </strong>John Doe</p>
+            <p><strong>Date of Birth: </strong>01/01/1990</p>
+            <p><strong>Political Party: </strong>Democrat</p>
+            <p><strong>Current Job: </strong>State Senator</p>
+            <p><strong>District Number: </strong>1</p>
+            <p><strong>State: </strong>California</p>
+            <select value={this.state.selectedOpinion} onChange={(e) => this.setState({ selectedOpinion: e.target.value })}>
+              <option value="">Political Opinions</option>
+              {this.state.opinions.map((opinion, index) => (
+                <option key={index} value={opinion}>{opinion}</option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div> 
+        );
+      }
 }
+
