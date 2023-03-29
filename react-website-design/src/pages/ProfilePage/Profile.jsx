440ba29932a9c22f931a2900ef9a5f687fead539
Abdel Rahman Mansour <65471490+Graycouch@users.noreply.github.com>
2023-03-28 23:44:05 -0500

    Worked on the profile page a little, still need to add editing functionality

--- react-website-design/src/pages/ProfilePage/Profile.jsx
+++ react-website-design/src/pages/ProfilePage/Profile.jsx
@@ -1,27 +1,79 @@
 import { useGlobalState } from '../../globalValues';
+import React, { useState } from 'react';
 import "./Profile.css";
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBTypography, MDBIcon, MDBBtn } from 'mdb-react-ui-kit';
 
 export default function Profile() {
   const [user] = useGlobalState("user");
+  const [userData, setUserData] = useState(user);
+
+  const editProfile = () => {
+    console.log("Hello");
+  }
 
   return (
-    <div className="Profile">
-      <div>
-        <p>
-          <img className="profile-picture" src="goat.png" alt="Profile" />
-        </p>
-      </div>
+    <section className="vh-100" style={{ backgroundColor: '#f4f5f7' }}>
+      <MDBContainer className="py-5 h-100">
+        <MDBRow className="justify-content-center align-items-center h-100">
+          <MDBCol lg="6" className="mb-4 mb-lg-0">
+            <MDBCard className="mb-3" style={{ borderRadius: '.5rem' }}>
+              <MDBRow className="g-0">
+                <MDBCol md="4" className="gradient-custom text-center text-white"
+                  style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }}>
 
-      <div className="profile-container">
-        <div className="profile-info">
-          <p><strong>Voter ID: </strong>{user.ID}</p>
-          <p><strong>Name: </strong>{user.first_name} {user.last_name}</p>
-          <p><strong>Date of Birth: </strong>{user.DOB}</p>
-          <p><strong>Political Party: </strong>{user.party_ID}</p>
-          <p><strong>District Number: </strong>{user.district_number}</p>
-          <p><strong>State: </strong>{user.state}</p>
-        </div>
-      </div>
-    </div>
+                  <MDBCardImage src="/Images/default.png" className="profile-picture" fluid />
+                  <MDBTypography tag="h5">{user.first_name} {user.last_name}</MDBTypography>
+                  <MDBBtn className='my-4' outline color="light" style={{ height: '36px', overflow: 'visible' }} onClick={() => editProfile()}>
+                    Edit profile
+                  </MDBBtn>
+                </MDBCol>
+
+                <MDBCol md="8">
+                  <MDBCardBody className="p-4">
+
+                    <MDBTypography tag="h6">Information</MDBTypography>
+                    <hr className="mt-0 mb-4" />
+
+                    <MDBRow className="pt-1">
+                      <MDBCol size="6" className="mb-3">
+                        <MDBTypography tag="h6">SSN</MDBTypography>
+                        <MDBCardText className="text-muted">{userData.SSN}</MDBCardText>
+                      </MDBCol>
+                      <MDBCol size="6" className="mb-3">
+                        <MDBTypography tag="h6">Birthday</MDBTypography>
+                        <MDBCardText className="text-muted">{userData.DOB}</MDBCardText>
+                      </MDBCol>
+                    </MDBRow>
+
+                    <MDBRow className="pt-1">
+                      <MDBCol size="6" className="mb-3">
+                        <MDBTypography tag="h6">State</MDBTypography>
+                        <MDBCardText className="text-muted">{user.state}</MDBCardText>
+                      </MDBCol>
+                      <MDBCol size="6" className="mb-3">
+                        <MDBTypography tag="h6">District Number</MDBTypography>
+                        <MDBCardText className="text-muted">{user.district_number}</MDBCardText>
+                      </MDBCol>
+                    </MDBRow>
+
+                    <MDBRow className="pt-1">
+                      <MDBCol size="6" className="mb-3">
+                        <MDBTypography tag="h6">Email</MDBTypography>
+                        <MDBCardText className="text-muted">{userData.email}</MDBCardText>
+                      </MDBCol>
+                      <MDBCol size="6" className="mb-3">
+                        <MDBTypography tag="h6">Phone Number</MDBTypography>
+                        <MDBCardText className="text-muted">{user.phone_number}</MDBCardText>
+                      </MDBCol>
+                    </MDBRow>
+
+                  </MDBCardBody>
+                </MDBCol>
+              </MDBRow>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+    </section>
   )
 }
